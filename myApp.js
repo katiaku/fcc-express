239a46ccc385118b38e14fa22efb58ce52f46191
Katia <kushnir.ekaterina@gmail.com>
2023-06-09 12:51:06 +0200

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
+// console.log("Hello World");
+
+app.use(express.static(__dirname + "/public"));
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-    res.send("Hello Express");
+    // res.send("Hello Express");
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
