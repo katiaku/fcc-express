d46219fe77c4f7704a4039f1176a8c7ea89326c0
Katia <kushnir.ekaterina@gmail.com>
2023-06-10 15:08:24 +0200

    Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -1,16 +1,25 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
+// solves the problem with submitting the tasks
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first');
 
+// middleware
 app.use("/", function(req, res, next) {
     let str = req.method + " " + req.path + " - " + req.ip;
     console.log(str);
     next();
 });
 
-require('dotenv').config();
+app.get("/now", function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({time: req.time});
+});
 
 // console.log("Hello World");
 
