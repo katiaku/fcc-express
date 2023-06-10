44692d1f00b7ba07dee71f3907917ad1b5ef5521
Katia <kushnir.ekaterina@gmail.com>
2023-06-10 15:17:38 +0200

    Get Route Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -43,4 +43,9 @@ app.get("/json", (req, res) => {
     }
 });
 
+app.get("/:word/echo", function(req, res) {
+    const { word } = req.params;
+    res.json({echo: word});
+});
+
 module.exports = app;
